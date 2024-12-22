dfc0fd0d6bbc78b54586fa0d0cb4b4abce165da6
Hasan Corley <75766051+Crzzy98@users.noreply.github.com>
2024-12-22 06:05:49 -0500

    Initial commit, add login component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,6 +48,7 @@ export default function RootLayout() {
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
+  //Routes defined
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
